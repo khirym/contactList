33e7859102099c096d22673159805b1d7fcdacb6
Khiry Maynard <khirymaynard@macbookproislike.local>
2023-12-22 06:51:48 -0800

    revisiting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import ContactList from "./components/ContactList";
+import { useState } from "react";
+import SelectedContact from "./components/SelectedContact";
 
-function App() {
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-   
+      {selectedContactId ? <div>Selected Contact View</div> : <ContactList />}
     </>
-  )
+  );
 }
-
-export default App
